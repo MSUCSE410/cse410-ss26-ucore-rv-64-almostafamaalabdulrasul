d1247864d58bff252def85e1bee35f50ac204d9f
Almostafa Aalabdulrasul <115664575+almostafamaalabdulrasul@users.noreply.github.com>
2026-02-05 18:00:59 +0000

    Chapter 3 Submission

--- os/syscall.c
+++ os/syscall.c
@@ -4,6 +4,7 @@
 #include "syscall_ids.h"
 #include "timer.h"
 #include "trap.h"
+#include "proc.h"
 
 uint64 sys_write(int fd, char *str, uint len)
 {
@@ -39,6 +40,22 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 /*
 * LAB1: you may need to define sys_task_info here
 */
+uint64 sys_task_info(TaskInfo *ti)
+{
+    struct proc *p = curr_proc();
+
+    ti->status = Running;
+
+    for (int i = 0; i < MAX_SYSCALL_NUM; i++) {
+        ti->syscall_times[i] = p->syscall_times[i];
+    }
+
+    uint64 now = get_cycle();
+    uint64 start = p->start_cycle;
+
+    ti->time = (int)((now - start) * 1000 / CPU_FREQ);
+    return 0;
+}
 
 extern char trap_page[];
 
@@ -53,6 +70,10 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	if (id >= 0 && id < MAX_SYSCALL_NUM) {
+		curr_proc()->syscall_times[id]++;
+	}
+
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -65,6 +86,9 @@ void syscall()
 		break;
 	case SYS_gettimeofday:
 		ret = sys_gettimeofday((TimeVal *)args[0], args[1]);
+		break;
+	case SYS_task_info:
+		ret = sys_task_info((TaskInfo *)args[0]);
 		break;
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
